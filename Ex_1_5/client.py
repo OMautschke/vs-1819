1bcb2c4f31f62a246e468b7ad265abe685dbb978
Oliver Mautschke <olivermautschke@gmail.com>
2018-10-26 00:55:37 +0200

    Straight outa atom

--- Ex_1_5/client.py
+++ Ex_1_5/client.py
@@ -9,6 +9,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         s.sendall(b'Hello, world')
         data = s.recv(1024)
-        
+
 print('Received', repr(data))
 
+// TEST
