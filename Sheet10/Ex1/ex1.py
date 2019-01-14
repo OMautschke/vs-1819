8eb711b5688076dac640eb276e9be72daae25dd5
Sebastian <sebastian.hegmann@gmx.de>
2019-01-14 22:02:24 +0100

    works?

--- Sheet10/Ex1/ex1.py
+++ Sheet10/Ex1/ex1.py
@@ -4,9 +4,11 @@ import sys
 import time
 from random import randint
 
+
 class ProcessState(Enum):
     ACTIVE = 1
     RELAY = 2
+
 
 class Process:
     UID = 0
@@ -38,57 +40,60 @@ class Process:
                 q.append(self.UID_temp)
             else:
                 q.append(self.UID_temp_n)
-    
+        return elected
+
     def final(self, elected):
         if self.UID_temp_nn == self.UID:
             elected = self.UID
         else:
-            if self.UID_temp_n > self.UID_temp_nn and self.UID_temp_n > self.UID_temp:
+            if self.UID_temp_n >= self.UID_temp_nn and self.UID_temp_n >= self.UID_temp:
                 self.UID_temp = self.UID_temp_n
             else:
                 self.State = ProcessState.RELAY
+        return elected
 
     def getState(self):
         return self.State
-  
+
+
 def main():
     print("Simulation started")
     print("Init...")
     id = 0
     elected = -1
     sharedMemory = deque([])
-    numProcesses = int(sys.argv[1])
+    numProcesses = 7
     processes = []
-    for i in range (0, numProcesses):
+    for i in range(0, numProcesses):
         processes.append(Process(id))
         id = id + 1
 
     while True:
         processes[0].send(sharedMemory)
-        for p in range (1, numProcesses):
+        for p in range(1, numProcesses):
             processes[p].receive(sharedMemory, 1)
             processes[p].send(sharedMemory)
         processes[0].receive(sharedMemory, 1)
 
-        processes[0].checkSend(sharedMemory, elected)
-        for p in range (1, numProcesses):
+        elected = processes[0].checkSend(sharedMemory, elected)
+        for p in range(1, numProcesses):
             processes[p].receive(sharedMemory, 2)
-            processes[p].checkSend(sharedMemory, elected)
+            elected = processes[p].checkSend(sharedMemory, elected)
             if elected > -1:
                 break
         if elected > -1:
             break
         processes[0].receive(sharedMemory, 2)
 
-        for p in range (0, numProcesses):
-            processes[p].final(elected)
+        for p in range(0, numProcesses):
+            elected = processes[p].final(elected)
             if elected > -1:
                 break
         if elected > -1:
             break
     print("Process " + str(elected) + " was elected!")
-            
 
 
 if __name__ == "__main__":
     main()
+
