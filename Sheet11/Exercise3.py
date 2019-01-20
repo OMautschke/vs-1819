b40c91aa1b80855eb93c49b29d2c2e4faee6a610
Sebastian <sebastian.hegmann@gmx.de>
2019-01-20 19:47:22 +0100

    Ex03

---
+++ Sheet11/Exercise3.py
@@ -0,0 +1,75 @@
+import time
+import datetime
+import hashlib
+
+
+LEADING_ZEROS = 3
+NUM_OF_BLOCKS = 100
+
+
+class Block:
+    def __init__(self, index):
+        self.index = index
+        self.time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S')
+        self.nounce = 0
+        self.prev_hash = 1
+
+        self.proof_of_work()
+
+    def proof_of_work(self):
+        global NUM_OF_BLOCKS, LEADING_ZEROS
+        self.nounce = 100 + self.index
+        D = "0" * LEADING_ZEROS
+
+        while True:
+            hash = hashlib.sha256(("Hello World!" + str(self.nounce)).encode('utf-8'))
+
+            if D in str(hash.hexdigest()[:LEADING_ZEROS]):
+                self.prev_hash = hash
+                break
+            self.nounce += 1
+
+
+def main():
+	# Part 1
+    global NUM_OF_BLOCKS, LEADING_ZEROS
+    block_chain = []
+
+    guesses = 0
+    start = time.time()
+    for i in range(0, NUM_OF_BLOCKS):
+        block_chain.append(Block(len(block_chain)))
+        guesses += block_chain[i].nounce
+    end = time.time()
+
+    print("Average guesses Blocks = 100; D = 3: " + str(int(guesses / NUM_OF_BLOCKS)))
+    print("Time elapsed: " + str(end - start) + "\n")
+
+    # Part 2
+    block_chain = []
+    NUM_OF_BLOCKS = 10
+    LEADING_ZEROS = 4
+
+    guesses = 0
+    for i in range(0, NUM_OF_BLOCKS):
+        block_chain.append(Block(len(block_chain)))
+        guesses += block_chain[i].nounce
+
+    print("Average guesses Blocks = 10; D = 4: " + str(int(guesses / NUM_OF_BLOCKS)) + "\n")
+
+    ####################################################################################
+
+    block_chain = []
+    NUM_OF_BLOCKS = 10
+    LEADING_ZEROS = 5
+
+    guesses = 0
+    for i in range(0, NUM_OF_BLOCKS):
+        block_chain.append(Block(len(block_chain)))
+        guesses += block_chain[i].nounce
+
+    print("Average guesses Blocks = 10; D = 5: " + str(int(guesses / NUM_OF_BLOCKS)) + "\n")
+
+
+if __name__ == "__main__":
+    main()
